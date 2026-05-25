4be16e9ddad6d3dbf8f7f1639f9795ac2ef7f8ef
Даниил Лопухов <lopukhov690@gmail.com>
2026-05-25 14:38:01 +0300

    Убрал лишние пробелы по PEP8

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Добавили v1, как требует документация
     path('api/v1/', include('api.urls')),
-    
+
     # Добавили маршруты для работы с JWT-токенами
     path('api/v1/', include('djoser.urls.jwt')),
-    
+
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
